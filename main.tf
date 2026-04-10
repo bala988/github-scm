e3c6bf959873865534ac7bc43b0b4e08bc5c758e
Balasuriyabala <balasuriyamech15@gmail.com>
2026-04-10 15:25:17 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
-resource "aws_instance" "name" {
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.allow_ssh.name]
+
+  tags = {
+    Name = "VPC Instance"
+  }
   
 }
